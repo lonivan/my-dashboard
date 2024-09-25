ac9abb8ca2564c6123137d31d82ae5abd4cecbcb
lonivan <lonivan.dev@gmail.com>
2024-09-25 10:03:04 +0200

    Add authentication

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -51,7 +51,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               {error}
             </p>
           ))}
-      </div>
+      </div> 
         </div>
 
         {/* Invoice Amount */}
